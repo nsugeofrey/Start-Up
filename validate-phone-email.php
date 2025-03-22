7ecf21f3406c3900841e89b3a047c657586c49cc
nsugeofrey <81487176+nsugeofrey@users.noreply.github.com>
2025-03-22 20:51:44 +0400

    Update and rename validating Phone Numbers to validate-phone-email.php

--- validate-phone-email.php
+++ validate-phone-email.php
@@ -4,12 +4,25 @@
 //Regular expression - the code considers two prefixes ie (058, 056)
 ^05[8|6]\s[0-9]{4}\s[0-9]{3}$
 <?php
-function testingreg($val){
+function validatePhone($val){
   if(preg_match("/^05[8|6]\s[0-9]{4}\s[0-9]{3}$/", $val)){
     echo $val;
   }
   else{
     echo "Enter Valid Number";
+  }
+}
+  ?>
+
+
+//this code is regular expression to validate email addresses
+
+function validateEmail($val){
+  if(preg_match("/^[a-zA-Z0-9_]+@+[0-9a-zA-Z_]+\.[a-zA-Z]{2,5}$/", $val)){
+    echo $val;
+  }
+  else{
+    echo "Enter Valid Email";
   }
 }
   ?>
